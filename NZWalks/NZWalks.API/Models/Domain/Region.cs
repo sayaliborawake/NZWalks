30f9a6a5943652f05e24381e3ab60fd1035604f3
sayali.borawake <borawakesayali@gmail.com>
2023-02-21 15:48:23 +0530

    End of section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string  Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        // Navigation property
+        public IEnumerable<Walk> Walks { get; set; } //One region can have multiple walks
+    }
+}
