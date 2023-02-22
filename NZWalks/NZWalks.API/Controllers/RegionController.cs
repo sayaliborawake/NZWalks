f621303c56967fa16b710d3a581a554148865ec1
sayali.borawake <borawakesayali@gmail.com>
2023-02-22 15:23:55 +0530

    End of session 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("Regions")]
+    public class RegionController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly IRegionRepository regionRepository;
+
+        public RegionController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllRegions() 
+        {
+            var regions = regionRepository.GetAllAsync();
+
+            // Return DTO regions
+            //var RegionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id= region.Id,
+            //        Name = region.Name,    
+            //        Area= region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population= region.Population
+            //    };
+            //});
+            var RegionsDTO =  mapper.Map<List<Models.DTO.Region>>(regions);
+
+            return Ok(RegionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        public RegionRepository(NZWalksDBContext dBContext) => DBContext = dBContext;
+        public NZWalksDBContext DBContext { get; }
+
+
+        public async Task<IEnumerable<Region>> GetAllAsync() => await DBContext.Regions.ToListAsync();
+    }
+}
